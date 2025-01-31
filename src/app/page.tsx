e713699577df55d72cf3401ca3e5abc0698b3230
Aman Jindal <amanjindal174@gmail.com>
2025-01-31 22:52:48 +0530

    &apos; used

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to My Portfolio</h1>
-      <p>Hi, I'm a software developer passionate about building web applications.</p>
+      <p>Hi, I&apos;m a software developer passionate about building web applications.</p>
     </div>
   );
 }
