bdab27b9968c4b1a6f2eaedcba87d53f88b3fcca
Aman Jindal <amanjindal174@gmail.com>
2025-02-04 15:03:56 +0530

    useState removed in desktop layout

--- src/components/DesktopLayout.js
+++ src/components/DesktopLayout.js
@@ -1,7 +1,6 @@
 // src/components/MobileLayout.js
 "use client" // uses client-side state
 
-import { useState } from "react";
 import Link from "next/link";
 
 export default function DesktopLayout({ children }) {
